565d156fb289fba5e6b0fc27b71552c1cf6e8c8a
Eline van der Gaast <elinevdgaast@gmail.com>
2015-03-17 11:47:15 -0700

    Navbar styling

---
+++ main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+  moveScroller();
+});
+
+window.moveScroller = function() {
+  // var $navbar = $('#main-nav');
+  // var header_height = $('header').height();
+  // var nav_height = $navbar.height();
+  // var move = function () {
+  //   var window_top = $(window).scrollTop();
+  //   if (window_top > header_height - nav_height) {
+  //     $navbar.addClass('fixed');
+  //   } else if (window_top < header_height - nav_height) {
+  //     $navbar.removeClass('fixed');
+  //   }
+  // };
+
+  // $(window).on('touchmove',move);
+  // $(window).scroll(move);
+  // move();
+}
